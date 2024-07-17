91dc03d13040f24c289542e707e4ab7e1f3a6e86
gderallerechte <Gunther.Wiebe@uni-bayreuth.de>
2024-07-17 12:11:39 +0200

    Revert "Update DoublyLinkedList.java"
    This reverts commit 3f298a6dc9f0b70d12da0ea181abd97fff5f818f.

--- src/main/java/de/unibayreuth/se/teaching/list/DoublyLinkedList.java
+++ src/main/java/de/unibayreuth/se/teaching/list/DoublyLinkedList.java
@@ -76,9 +76,9 @@ public class DoublyLinkedList {
      * @return Array with list elements (same order)
      */
     public double[] asArray() {
-        double[] array = new double[length];
+        double[] array = new double[length+1];
         Element element = begin;
-        int arrayPos = 0;
+        int arrayPos = 1;
         while (element != null) {
             array[arrayPos] = element.value;
             arrayPos++;
@@ -150,8 +150,26 @@ public class DoublyLinkedList {
         }
     }
 
-
-
+    /**
+     * Add an element.
+     * @param e Element to add
+     */
+    public void add(Element e) {
+        if (isEmpty()) { append(e); }
+        else {
+            Element pos = begin; Element pred = null;
+            while (pos != null && pos.getValue() < e.getValue()) {
+                pred = pos; pos = pos.getNext();
+            }
+            if (pos == null) { append(e); }
+            else {
+                e.setNext(pos); pos.setPrev(e);
+                if (pred != null) { e.setPrev(pred); pred.setNext(e); }
+                else { begin = e; }
+                length++;
+            }
+        }
+    }
 
     /**
      * Inner class for doubly linked list elements
